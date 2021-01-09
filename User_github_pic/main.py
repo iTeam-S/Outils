7c06c12da47420632b5aa42bfd3267c98b0a3f6b
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2021-01-09 20:46:32 +0300

    Optimisation qualitÃ©code
    - Nom de variable interdite remplacÃ©
    - espacement ajuster
    - beautify sql code

--- User_github_pic/main.py
+++ User_github_pic/main.py
@@ -1,25 +1,23 @@
-import requests
-import mysql.connector
+import requests, mysql.connector
 from bs4 import BeautifulSoup
 from config import DATABASE
 
+
 def add_user_github_pic():
     db = mysql.connector.connect(**DATABASE)
-
     cursor = db.cursor()
 
-    cursor.execute("SELECT id, user_github FROM membre WHERE user_github IS NOT NULL AND user_github_pic IS NULL;")
+    cursor.execute("""
+        SELECT id, user_github FROM membre 
+        WHERE user_github IS NOT NULL AND user_github_pic IS NULL
+    """)
 
-    result = cursor.fetchall()
-
-    for tuple in result:
-        print('\n[INFO] ', tuple[0], '=>', tuple[1])
-        cursor.execute('UPDATE membre SET user_github_pic=%s WHERE id=%s', (get_user_github_pic('https://github.com/'+tuple[1]), tuple[0]))
-        
+    for val in cursor.fetchall():
+        print('\n[INFO] ', val[0], '=>', val[1])
+        cursor.execute('UPDATE membre SET user_github_pic=%s WHERE id=%s', (get_user_github_pic('https://github.com/'+val[1]), val[0]))
     db.commit()
-
-    cursor.close()
     db.close()
+
 
 def get_user_github_pic(user_github_url):
     r = requests.get(user_github_url)
@@ -27,9 +25,10 @@ def get_user_github_pic(user_github_url):
     print('[STATUS] : ', r.status_code)
 
     src_code = BeautifulSoup(r.text,'html.parser')
-
     for src in src_code.find_all(alt='Avatar'):
         img_link = src.get('src')
         return img_link[:img_link.find('?')]
 
-add_user_github_pic()
+
+if __name__ == '__main__':
+    add_user_github_pic()
