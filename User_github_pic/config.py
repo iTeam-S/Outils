a05c59e814161292f222671d6e923cc9c4254873
Arlème <60097202+rootkit7628@users.noreply.github.com>
2021-01-09 19:26:14 +0100

    Remove import

--- User_github_pic/config.py
+++ User_github_pic/config.py
@@ -1,8 +1,6 @@
-import mysql.connector
-
 DATABASE = {
-  'host' : "185.98.128.90",
-  'user' : "iteams",
-  'password' : "__iteam-s__",
-  'database' : "ITEAMS"
+  'host' : "",
+  'user' : "",
+  'password' : "",
+  'database' : ""
 }
