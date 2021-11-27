f7cb53b8dc7e6f5da6204ad03e699e7208606089
Gaetan Jonathan <gaetan.s118@gmail.com>
2021-11-27 08:46:22 +0300

    Optimize config_account
    add to readme
    grant privilege root to run script on server
    fix path scritp run, not found

--- Scripts/config_account/run.sh
+++ Scripts/config_account/run.sh
@@ -1,1 +1,7 @@
-ssh iteams@iteam-s.mg 'bash -s' < script.sh
+#/bin/bash
+
+# recuperation de l'emplacement du script en cours
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# lancement du script via ssh
+ssh root@iteam-s.mg 'bash -s' < $SCRIPT_DIR/script.sh $@
--- Scripts/config_account/script.sh
+++ Scripts/config_account/script.sh
@@ -9,4 +9,4 @@ useradd -m $username
 echo "$username:password" | chpasswd
 
 # Creation compte mysql avec ermission DB ITEAMS
-mysql -u iteams -p$ITEAMS_PASS -e "GRANT ALL PRIVILEGES ON ITEAMS.* TO $username@'%' identified by \"$password\";"
+mysql -e "GRANT ALL PRIVILEGES ON ITEAMS.* TO $username@'%' identified by \"$password\";"
