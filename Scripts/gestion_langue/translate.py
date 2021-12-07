2541b1358e78feac3b19bd408670c6853ad64989
rootkit7628 <arleme.dev7@gmail.com>
2021-12-08 01:51:47 +0300

    [QA] Inspection de code

--- Scripts/gestion_langue/translate.py
+++ Scripts/gestion_langue/translate.py
@@ -1,14 +1,8 @@
 import json
 
+
 def get(key, lang):
-	fichier=open("langs.json")
-	trans=json.load(fichier)
-	mot_cle=trans.get(key)
-
-	if mot_cle:
-		return mot_cle.get(lang) \
-	    	if mot_cle.get(lang) else key
-	else:
-		return key
-
-	return trans.get(key).get(lang)
+    fichier = open("langs.json")
+    mot_cle = json.load(fichier).get(key)
+    print(mot_cle)
+    return mot_cle.get(lang) or key if mot_cle else key
